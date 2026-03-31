5e1f8dab156163e8931e60fde6ae50aba80c3f63
yuzhenglong1 <63049471+yuzhenglong1@users.noreply.github.com>
2026-03-31 14:51:48 +0800

    Delete diy-part1.sh

--- diy-part1.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Add a feed source
-# 使用更稳定的插件源组合，适配最新 OpenWrt 官方源码
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
